ddd08d125cea0903dcfd901738970890b7001001
Brett Willis <brettjohnwillis@gmail.com>
2025-05-15 20:21:18 +1200

    Fix formatting

--- index.ts
+++ index.ts
@@ -19,14 +19,14 @@ export function expect(): PluginFn {
     TestContext.getter('expect', () => jestExpect, true)
 
     Test.executed(function (_, hasError) {
-      // Must call this whether or not the test passed because internally it 
+      // Must call this whether or not the test passed because internally it
       // resets the assertion count and otherwise the assertion count will be
       // carried into a subsequent test
-      const jestErrors = jestExpect.extractExpectedAssertionsErrors();
+      const jestErrors = jestExpect.extractExpectedAssertionsErrors()
 
       // Throw assertion count errors only if the test otherwise passed
       if (jestErrors.length && !hasError) {
-        throw jestErrors[0].error;
+        throw jestErrors[0].error
       }
     })
   }
